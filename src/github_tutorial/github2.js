ddd290bf1726c41c39b127d717d33ef2818beb7b
Arkaprabha Das <arkaprabha31@gmail.com>
2025-02-06 20:36:57 +0530

    currying function added in feature 2 branch

--- src/github_tutorial/github2.js
+++ src/github_tutorial/github2.js
@@ -16,3 +16,14 @@ function feature2()
     console.log("feature2 !!!")
     console.log("Feature 2 add on 1")
 }
+
+
+const sum  =  num1 => num2 => num3 => {
+    return num1+num2+num3;
+}
+
+const ans1 = sum(1);
+const ans2 = ans1(2);
+const ans3 = ans2(3);
+
+console.log(ans3);
